1de9c725094856120db51f589be68a1dececeef2
ErzaFoxy <ErzaFoxy55565@gmail.com>
2020-11-21 20:46:08 +0200

    test

--- src/js/script.js
+++ src/js/script.js
@@ -180,10 +180,11 @@ $(document).ready(function() {
 
     $('#viberButton').on('click', function() {
         var md = new MobileDetect(window.navigator.userAgent);
-        if (md.mobile()) {
-            location.href = 'viber://add?number=+380971535340'
-        } else {
-            location.href = 'viber://chat?number=+380971535340'
-        }
+        location.href = 'viber://add?number=+380971535340'
+            /*if (md.mobile()) {
+                location.href = 'viber://add?number=+380971535340'
+            } else {
+                location.href = 'viber://chat?number=+380971535340'
+            }*/
     });
 });
